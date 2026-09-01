5b4280116e4513a4757d61ad3e6ded5feee1429b
Vu Hoang An Nam ( annampc ) <vuhoangannam04@gmail.com>
2026-09-01 22:22:32 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,20 +5,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    open: true,
-    host: true
+    open: true
   },
   build: {
     outDir: 'dist',
-    sourcemap: false,
-    minify: 'terser',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          firebase: ['firebase/app', 'firebase/auth', 'firebase/firestore']
-        }
-      }
-    }
+    sourcemap: false
   }
 })
