5e02769a7b5dd422acb9ba93980e052cb5cf8056
Vu Hoang An Nam ( annampc ) <vuhoangannam04@gmail.com>
2026-09-02 14:14:06 +0700

    Update Setting.jsx

--- src/components/Setting.jsx
+++ src/components/Setting.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import toast from 'react-hot-toast';
 
 const Settings = ({ isOpen, onClose, user, logout, theme, setTheme, language, setLanguage }) => {
